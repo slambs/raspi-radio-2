cfcf403505bd34f04a590c1283d3d05f1ee58a7b
slambs <vasileios.selamis@gmail.com>
2025-08-05 22:22:24 +0200

    improve html and js

--- packages/src/index.js
+++ packages/src/index.js
@@ -1,9 +1,12 @@
 // Logic for displaying a clock
+function drawClock() {
+    const clock = document.getElementById('largeClock');
 
-const clock = document.getElementById('largeClock');
-
-function drawClock(){
-    clock.textContent = new Date().toLocaleTimeString();
+    if (clock) {
+        clock.textContent = new Date().toLocaleTimeString();
+    }
 }
-  
+
+drawClock();
+
 setInterval(drawClock, 1000);
